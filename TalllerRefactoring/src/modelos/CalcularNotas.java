cf5e036236deb5303dc14e4c6561b5ea5283e532
RicardoVilcacundo <rvilcacu@espol.edu.ec>
2020-08-13 15:33:47 -0500

    Solucionando code smell Large class y Long parameter list

---
+++ TalllerRefactoring/src/modelos/CalcularNotas.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ *
+ * @author ricar
+ */
+public class CalcularNotas {
+    Estudiante e;
+
+    public CalcularNotas(Estudiante e) {
+        this.e = e;
+    }
+    //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
+    public double CalcularNotaInicial(Paralelo p, double nexamen,double ndeberes, double nlecciones, double ntalleres){
+        double notaInicial=0;
+        for(Paralelo par:e.getParalelos()){
+            if(p.equals(par)){
+                double notaTeorico=(nexamen+ndeberes+nlecciones)*0.80;
+                double notaPractico=(ntalleres)*0.20;
+                notaInicial=notaTeorico+notaPractico;
+            }
+        }
+        return notaInicial;
+    }
+    
+    //Calcula y devuelve la nota final contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
+    
+    public double CalcularNotaFinal(Paralelo p, double nexamen,double ndeberes, double nlecciones, double ntalleres){
+        double notaFinal=0;
+        for(Paralelo par:e.getParalelos()){
+            if(p.equals(par)){
+                double notaTeorico=(nexamen+ndeberes+nlecciones)*0.80;
+                double notaPractico=(ntalleres)*0.20;
+                notaFinal=notaTeorico+notaPractico;
+            }
+        }
+        return notaFinal;
+    }
+    
+    //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. Esta nota es solo el promedio de las dos calificaciones anteriores.
+    public double CalcularNotaTotal(Paralelo p){
+        double notaTotal=0;
+        for(Paralelo par:e.getParalelos()){
+            if(p.equals(par)){
+                notaTotal=(p.getMateria().notaInicial+p.getMateria().notaFinal)/2;
+                
+            }
+        }
+        return notaTotal;
+        
+    }
+}
--- TalllerRefactoring/src/modelos/Profesor.java
+++ TalllerRefactoring/src/modelos/Profesor.java
@@ -3,26 +3,23 @@ package modelos;
 import java.util.ArrayList;
 
 public class Profesor {
-    public String codigo;
-    public String nombre;
-    public String apellido;
-    public int edad;
-    public String direccion;
-    public String telefono;
-    public InformacionAdicionalProfesor info;
-    public ArrayList<Paralelo> paralelos;
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String direccion;
+    private String telefono; 
+    private ArrayList<Paralelo> paralelos;
     private int añosdeTrabajo;
     private String facultad;
     private double BonoFijo;
 
-    public Profesor(String codigo, String nombre, String apellido, String facultad, int edad, String direccion, String telefono) {
+    public Profesor(String codigo, String nombre, String apellido, String facultad) {
         this.codigo = codigo;
         this.nombre = nombre;
         this.apellido = apellido;
-        this.edad = edad;
-        this.direccion = direccion;
-        this.telefono = telefono;
-        paralelos= new ArrayList<>();
+        this.facultad= facultad;
+       
     }
     
     public void anadirParalelos(Paralelo p){
